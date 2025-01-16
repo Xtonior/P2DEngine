894d40a969c7fda29cf4fea346972235dbb1c0fb
Xtonior <83264344+Xtonior@users.noreply.github.com>
2025-01-16 15:55:27 +0500

    2D top-down map

--- Assets/Shaders/shader.frag
+++ Assets/Shaders/shader.frag
@@ -4,21 +4,61 @@ in vec2 texCoord;
 
 out vec4 FragColor;
 
-uniform float screenX;
-uniform float sliceStart;
-uniform float sliceEnd;
-uniform vec3 wallColor;
+// Uniforms
+uniform vec2 u_resolution;
+uniform vec2 u_cameraPos;
+uniform vec2 u_cameraDir;
+
+uniform vec2 u_rayStart;
+uniform vec2 u_rayEnd;
+
+uniform sampler2D mapTexture;
 
 void main() 
 {
-    if (texCoord.x >= sliceStart && texCoord.x <= sliceEnd) 
+    vec2 uv = texCoord;
+    float pixelThickness = 1.0 / u_resolution.y;
+
+    float px = u_cameraPos.x / u_resolution.x; 
+    float py = u_cameraPos.y / u_resolution.y; 
+
+    float mapTex = texture(mapTexture, uv).r;
+    vec3 col = vec3(mapTex);
+
+    float playerThickness = 5.0;
+
+    if (abs(uv.x - px) < pixelThickness * playerThickness * 0.5 &&
+        abs(uv.y - py) < pixelThickness * playerThickness * 0.5) 
     {
-        // .x * texCoord.y, wallColor.y * texCoord.y, wallColor.z * texCoord.y
-        //  && texCoord.x == screenX
-        FragColor = vec4(wallColor, 1.0);
-    } 
-    else 
-    {
-        FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        col = vec3(1.0, 0.7, 0.3);
     }
+
+    float rayThickness = 2.0;
+
+    float rx1 = u_rayStart.x / u_resolution.x; 
+    float ry1 = u_rayStart.y / u_resolution.y; 
+
+    float rx2 = u_rayEnd.x / u_resolution.x; 
+    float ry2 = u_rayEnd.y / u_resolution.y; 
+
+    // Calculate slope and intercept of the line
+    float m = (ry2 - ry1) / (rx2 - rx1); // slope
+    float b = ry1 - m * rx1; // y-intercept
+    
+    // Ensure rx1 is the smaller value and rx2 is the larger value
+    float startX = min(rx1, rx2);
+    float endX = max(rx1, rx2);
+    
+    // Loop from startX to endX
+    for (float rx = startX; rx <= endX; rx += pixelThickness)
+    {
+        float ry = m * rx + b; // calculate the y position for this x
+    
+        if (abs(uv.x - rx) < pixelThickness * rayThickness * 0.5 && abs(uv.y - ry) < pixelThickness * rayThickness * 0.5)
+        {
+            col = vec3(0.5, 0.1, 0.3); // Set color when in range
+        }
+    }   
+
+    FragColor = vec4(col, 1.0);
 }
--- Core/Window.cs
+++ Core/Window.cs
@@ -3,6 +3,7 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Mathematics;
+using System.Reflection.Metadata;
 
 namespace P2DEngine.Core
 {
@@ -26,12 +27,16 @@ namespace P2DEngine.Core
 
         private Shader mainShader;
 
-        private int[,] map =
+        private float[] map =
         {
-            {1, 1, 1, 1},
-            {1, 0, 0, 1},
-            {1, 0, 0, 1},
-            {1, 1, 1, 1},
+            1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 1,
+            1, 0, 0, 0, 0, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1
         };
 
         public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
@@ -73,6 +78,8 @@ namespace P2DEngine.Core
             GL.EnableVertexAttribArray(texCoordLocation);
             GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
 
+            CreateTex();
+
             // Unbind VBO and VAO
             // GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             // GL.BindVertexArray(0);
@@ -89,30 +96,18 @@ namespace P2DEngine.Core
 
             mainShader.Use();
 
-            if (x <= 300)
-            {
-                DrawVerticalSlice(x, 600, 800, 600);
-            }
-            if (x > 300)
-            {
-                DrawVerticalSlice(x, 600, 800, 600);
-                mainShader.SetVector3("wallColor", new Vector3(0.9f, 0.7f, 0.7f));
-            }
-
-            if (x < 800)
-            {
-                x++;
-            }
-            else
-            {
-                x = 0;
-            }
+            DrawVerticalSlice(x, 600, 800, 600);
 
             GL.BindVertexArray(vpVao);
             GL.DrawElements(BeginMode.Triangles, viewportIndices.Length, DrawElementsType.UnsignedInt, 0);
 
             SwapBuffers();
         }
+
+        float posX = 400, posY = 300;  //x and y start position
+        float angle;
+        Vector2 dir;
+        double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
@@ -124,6 +119,37 @@ namespace P2DEngine.Core
             {
                 Close();
             }
+
+            if (input.IsKeyDown(Keys.W))
+            {
+                posX += dir.X * 100 * (float)e.Time;
+                posY += dir.Y * 100 * (float)e.Time;
+            }
+            if (input.IsKeyDown(Keys.S))
+            {
+                posX -= dir.X * 100 * (float)e.Time;
+                posY -= dir.Y * 100 * (float)e.Time;
+            }
+
+            if (input.IsKeyDown(Keys.A))
+            {
+                angle += 5f * (float)e.Time;
+            }
+            if (input.IsKeyDown(Keys.D))
+            {
+                angle -= 5f * (float)e.Time;
+            }
+
+            if (angle < 0.0f)
+            {
+                angle = MathHelper.TwoPi;
+            }
+            if (angle > MathHelper.TwoPi)
+            {
+                angle = 0.0f;
+            }
+
+            dir = new Vector2((float)MathHelper.Cos(angle), (float)MathHelper.Sin(angle));
         }
 
         protected override void OnResize(ResizeEventArgs e)
@@ -146,61 +172,42 @@ namespace P2DEngine.Core
             base.OnUnload();
         }
 
-        double posX = 3, posY = 2;  //x and y start position
-        double dirX = -1, dirY = 0; //initial direction vector
-        double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+        int textureID;
+
+        void CreateTex()
+        {            
+            GL.GenTextures(1, out textureID);
+            GL.BindTexture(TextureTarget.Texture2D, textureID);
+
+            // Upload the texture data
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.R32f, 8, 8, 0, PixelFormat.Red, PixelType.Float, map);
+
+            // Set texture parameters
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
+
+            // Unbind the texture
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+        }
 
         void DrawVerticalSlice(float x, int wallHeight, int screenWidth, int screenHeight)
         {
-            int startY = (screenHeight - wallHeight) / 2;
-            int endY = startY + wallHeight;
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, textureID);
 
-            int start = (startY / endY);
-            int end = endY / endY;
+            // Draw map
+            int textureLocation = GL.GetUniformLocation(mainShader.Handle, "mapTexture");
+            GL.Uniform1(textureLocation, 0); // Texture unit 0
 
-            mainShader.SetFloat("sliceStart", 0.0f);
-            mainShader.SetFloat("sliceEnd", 0.5f);
-            mainShader.SetVector3("wallColor", new Vector3(0.3f, 0.5f, 0.7f));
-        }
+            // Draw Player
+            mainShader.SetVector2("u_resolution", new Vector2(ClientSize.X, ClientSize.Y));
+            mainShader.SetVector2("u_cameraPos", new Vector2(posX, posY));
+            //mainShader.SetVector2("u_cameraDir", new Vector2(1, 1));
 
-        void CastRays(float playerX, float playerY, float playerAngle, float fov)
-        {
-            float rayAngleStart = playerAngle - fov / 2;
-            float rayAngleStep = fov / ClientSize.X;
-
-            for (int x = 0; x < ClientSize.X; x++)
-            {
-                float rayAngle = rayAngleStart + x * rayAngleStep;
-
-                // Cast ray
-                float distanceToWall = 0;
-                bool hitWall = false;
-                while (!hitWall && distanceToWall < 150.0f)
-                {
-                    distanceToWall += 0.1f; // Increment ray
-
-                    // Calculate test coordinates
-                    int testX = (int)(playerX + MathF.Cos(rayAngle) * distanceToWall);
-                    int testY = (int)(playerY + MathF.Sin(rayAngle) * distanceToWall);
-
-                    // Check if ray is out of bounds
-                    if (testX < 0 || testY < 0 || testX >= map.GetLength(0) || testY >= map.GetLength(1))
-                    {
-                        hitWall = true;
-                        distanceToWall = 150.0f; // Hit wall at maximum depth
-                    }
-                    else if (map[testX, testY] == 1) // Hit a wall
-                    {
-                        hitWall = true;
-                    }
-                }
-
-                // Calculate wall height
-                int wallHeight = (int)(ClientSize.Y / distanceToWall);
-
-                // Draw vertical slice of wall
-                DrawVerticalSlice(x, wallHeight, ClientSize.X, ClientSize.Y);
-            }
+            mainShader.SetVector2("u_rayStart", new Vector2(posX, posY));
+            mainShader.SetVector2("u_rayEnd", 100f * dir + new Vector2(posX, posY)); 
         }
     }
 }
